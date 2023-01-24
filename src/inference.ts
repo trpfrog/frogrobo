5dea6b144f972cafaa9fb02b757e8c993ab3635b
TrpFrog <dev@trpfrog.net>
2023-01-25 02:07:56 +0900

    Remove unnecessary parameters in diffusionQuery

--- src/inference.ts
+++ src/inference.ts
@@ -28,7 +28,6 @@ export type TextGenerationPayload = InferencePayload & {
 
 export type TextToImagePayload = InferencePayload & {
   inputs: string
-  parameters?: TextGenerationParameters
 }
 
 export async function hfQuery (
