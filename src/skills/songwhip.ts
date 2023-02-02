2a2bace0c1e9d290da1343ef24037d9a151d79e5
TrpFrog <dev@trpfrog.net>
2023-02-03 02:41:24 +0900

    Implement songwhip reply fn

--- src/skills/songwhip.ts
+++ src/skills/songwhip.ts
@@ -9,6 +9,8 @@ export type SongwhipStreamingServices =
   | 'amazonMusic'
   | 'itunesStore'
   | 'youtubeMusic'
+  | 'spotify'
+  | 'lineMusic'
 
 export interface SongwhipStreamingLink {
   link: string
@@ -53,23 +55,32 @@ const fetchSongLink = async (songLink: string): Promise<SongwhipResult> => {
     },
     body: JSON.stringify({ url: songLink })
   })
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`)
+  }
   return await res.json()
 }
 
 const songwhipReply: AccountActivityListener = {
   onTweetCreated: async (tweet, social) => {
-    const id = tweet.id_str
-    const text = tweet.text
-    const songLinkRegex = /https?:\/\/open\.spotify\.com\/track\/[a-zA-Z0-9]+/
-    const songLink = text.match(songLinkRegex)
-
-    if (songLink == null) {
+    if (!tweet.text.toLowerCase().includes('#nowplaying')) {
       return false
     }
 
-    const link = songLink[0]
-    const songInfo = await fetchSongLink(link)
-    await social.reply(id, songInfo.url)
+    // eslint-disable-next-line no-irregular-whitespace
+    const probablySongLink = tweet.text.match(/https?:\/\/[^\s　]+/g)
+    if (probablySongLink == null) {
+      return false
+    }
+
+    const link = probablySongLink[0]
+    try {
+      const songInfo = await fetchSongLink(link)
+      const msg = `🎵 ${songInfo.artists[0].name} - ${songInfo.name}\n${songInfo.url}`
+      await social.reply(tweet.id_str, msg)
+    } catch (e) {
+      return false
+    }
     return true
   }
 }
