9572a34ce9b84ee7c5b6f83dbf0fd5de6f81892f
TrpFrog <dev@trpfrog.net>
2023-02-03 04:13:36 +0900

    Add token authorization

---
+++ src/auth.ts
@@ -0,0 +1,30 @@
+import type * as ff from '@google-cloud/functions-framework'
+import crypto from 'crypto'
+import { StatusCodes } from 'http-status-codes'
+
+export function webhookChallenge (req: ff.Request, res: ff.Response): void {
+  if (typeof req.params.crc_token !== 'undefined') {
+    const consumerSecret = JSON.parse(process.env.TWITTER_TOKEN_JSON ?? '').consumer_secret ?? ''
+
+    const sha256hashDigest = crypto
+      .createHmac('sha256', consumerSecret)
+      .update(req.body?.crc_token ?? '')
+      .digest('base64')
+
+    res.status(StatusCodes.OK)
+      .header('Content-Type', 'application/json')
+      .send({
+        response_token: `sha256=${sha256hashDigest}`
+      })
+  } else {
+    res.send({ error: 'No crc_token provided' })
+  }
+}
+
+export function isAccessAllowed (req: ff.Request): boolean {
+  if ('trpfrog-webhook-token' in req.params) {
+    return req.params['trpfrog-webhook-token'] === process.env.TRPFROG_WEBHOOK_TOKEN
+  } else {
+    return false
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,26 @@
 import * as ff from '@google-cloud/functions-framework'
 import 'dotenv/config'
-import * as crypto from 'crypto'
-import { StatusCodes } from 'http-status-codes'
 import { Bot } from './bot'
 import actions from './skills'
-
-function webhookChallenge (req: ff.Request, res: ff.Response): void {
-  if (typeof req.params.crc_token !== 'undefined') {
-    const consumerSecret = JSON.parse(process.env.TWITTER_TOKEN_JSON ?? '').consumer_secret ?? ''
-
-    const sha256hashDigest = crypto
-      .createHmac('sha256', consumerSecret)
-      .update(req.body?.crc_token ?? '')
-      .digest('base64')
-
-    res.status(StatusCodes.OK)
-      .header('Content-Type', 'application/json')
-      .send({
-        response_token: `sha256=${sha256hashDigest}`
-      })
-  } else {
-    res.send({ error: 'No crc_token provided' })
-  }
-}
+import { isAccessAllowed, webhookChallenge } from './auth'
 
 async function action (body: any): Promise<void> {
   const bot = new Bot()
-  actions.forEach(act => { bot.addListener(act) })
-  await bot.run(body)
 }
 
 ff.http('FrogRoboFunction', async (req: ff.Request, res: ff.Response) => {
-  // eslint-disable-next-line @typescript-eslint/no-base-to-string
-  // const params = req.params
+  if (!isAccessAllowed(req)) {
+    res.status(401)
+    res.send({ error: 'Unauthorized' })
+    return
+  }
   console.log(req.params)
 
   if (req.method === 'GET') {
     webhookChallenge(req, res)
   } else if (req.method === 'POST') {
-    await action(req.body)
+      actions.forEach(act => { bot.addListener(act) })
+      await bot.run(req.body)
   } else {
     res.send({ error: 'Invalid request' })
   }
